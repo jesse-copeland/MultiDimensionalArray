005871775539fe34886fda55789cf7a0447f29c7
Jesse Copeland <jesse.copeland@gmail.com>
2015-01-21 09:16:55 -1000

    Refresh for level 1 branch

--- MultiDimensionalArray.js
+++ MultiDimensionalArray.js
@@ -1,10 +1,1 @@
-module.exports = {
-  generate: function () {
-    var staticReturn = [
-      [0,1,0],
-      [1,1,0],
-      [0,1,1]
-    ];
-    return staticReturn;
-  }
-};
+
--- test/test-generator.js
+++ test/test-generator.js
@@ -1,19 +1,1 @@
-var chai = require('chai');
-var expect = chai.expect;
-var should = chai.should();
-var mda = require('../MultiDimensionalArray.js');
 
-describe("MultiDimensionalArray Tester", function() {
-  describe("Generator Function", function() {
-    it("should be defined as a Function", function() {
-      expect(mda.generate).to.be.a('function');
-      expect(mda.generate).to.be.instanceof(Function);
-    });
-    it("should return an array containing three arrays if no arguments are passed", function() {
-      var testArray = [[0,1,0],[1,1,0],[0,1,1]];
-
-      expect(mda.generate()).to.deep.equal(testArray);
-      
-    });
-  });
-});
