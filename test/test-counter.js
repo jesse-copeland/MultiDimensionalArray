79c1a55cc62aaf9bad4cb6b03a161bab5ca2aa0f
Jesse Copeland <jesse.copeland@gmail.com>
2015-01-22 12:17:30 -1000

    setup chai, no tests

--- test/test-counter.js
+++ test/test-counter.js
@@ -4,9 +4,9 @@ var should = chai.should();
 
 var mda = require('../MultiDimensionalArray.js');
 
-describe("Call MultiDimensionalArray.count method with ⇒", function() {
-  it("⇒ an argument containing an array. It should return a count of the number of truthy elements contained in the array.", function() {
-    var testArray = [1,0,0,1,0,0,0,0,1];
-    expect(mda.count(testArray)).to.equal(3);
-  });  
-});
+// describe("Call MultiDimensionalArray.count method with ⇒", function() {
+//   it("⇒ an argument containing an array. It should return a count of the number of truthy elements contained in the array.", function() {
+//     var testArray = [1,0,0,1,0,0,0,0,1];
+//     expect(mda.count(testArray)).to.equal(3);
+//   });  
+// });
