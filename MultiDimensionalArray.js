1ac02c91d4a7980bfadf054127e229c87f269816
Jesse Copeland <jesse.copeland@gmail.com>
2015-01-22 11:40:59 -1000

    Refactored validation checks and var name

--- MultiDimensionalArray.js
+++ MultiDimensionalArray.js
@@ -1,81 +1,57 @@
 module.exports = {
-  generate_1d: function (arrLenArg) {
+  generate_1d: function (g1dTier1Arg) {
     var result = [];
-    var arrLength = 3;
 
-    if (arrLenArg === undefined) {
-      // Do nothing when undefined.
-    } else if (typeof arrLenArg === 'number') {
-      arrLength = arrLenArg;
+    if (g1dTier1Arg === undefined) {g1dTier1Arg = 3;}
+
+    if (typeof g1dTier1Arg === 'number') {
+      g1dTier1Len = Math.abs(g1dTier1Arg);
     } else {
       return [];
     }
     
-    while (result.length < arrLength) {
+    while (result.length < g1dTier1Len) {
       result.push(this.random01());
     }
   
     return result;
   },
-  generate_2d: function (outLenArg, inLenArg) {
+  generate_2d: function (g2dTier1Arg, g2dTier2Arg) {
     var result = [];
-    var outerArrLength = 3;
-    var innerArrLength = 3;
 
-    if (outLenArg === undefined) {
-      // Do nothing when undefined.
-    } else if (typeof outLenArg === 'number') {
-      outerArrLength = outLenArg;
+    if (g2dTier1Arg === undefined) {g2dTier1Arg = 3;}
+    if (g2dTier2Arg === undefined) {g2dTier2Arg = 3;}
+    
+    if (typeof g2dTier1Arg === 'number' && typeof g2dTier2Arg === 'number') {
+      g2dTier1Len = Math.abs(g2dTier1Arg);
+      g2dTier2Len = Math.abs(g2dTier2Arg);
     } else {
       return [];
     }
 
-    if (inLenArg === undefined) {
-      // Do nothing when undefined.
-    } else if (typeof inLenArg === 'number') {
-      innerArrLength = inLenArg;
-    } else {
-      return [];
-    }
-
-    while (result.length < outerArrLength) {
-      result.push(this.generate_1d(innerArrLength));
+    while (result.length < g2dTier1Len) {
+      result.push(this.generate_1d(g2dTier2Len));
     }
 
     return result;
   },
-  generate_3d: function (outOutLenArg, outInLenArg, inInLenArg) {
+  generate_3d: function (g3dTier1Arg, g3dTier2Arg, g3dTier3Arg) {
     var result = [];
-    var outOuterArrLen = 3;
-    var outInnerArrLen = 3;
-    var inInnerArrLen = 3;
 
-    if (outOutLenArg === undefined) {
-      // Do nothing when undefined.
-    } else if (typeof outOutLenArg === 'number') {
-      outOuterArrLen = outOutLenArg;
+    if (g3dTier1Arg === undefined) {g3dTier1Arg = 3;}
+    if (g3dTier2Arg === undefined) {g3dTier2Arg = 3;}
+    if (g3dTier3Arg === undefined) {g3dTier3Arg = 3;}
+    
+    if (typeof g3dTier1Arg === 'number' && typeof g3dTier2Arg === 'number' && typeof g3dTier3Arg === 'number') {
+      g3dTier1Len = Math.abs(g3dTier1Arg);
+      g3dTier2Len = Math.abs(g3dTier2Arg);
+      g3dTier3Len = Math.abs(g3dTier3Arg);
     } else {
       return [];
     }
-
-    if (outInLenArg === undefined) {
-      // Do nothing when undefined.
-    } else if (typeof outInLenArg === 'number') {
-      outInnerArrLen = outInLenArg;
-    } else {
-      return [];
-    }
-
-    if (inInLenArg === undefined) {
-      // Do nothing when undefined.
-    } else if (typeof inInLenArg === 'number') {
-      inInnerArrLen = inInLenArg;
-    } else {
-      return [];
-    }
-
-    while (result.length < outOuterArrLen) {
-      result.push(this.generate_2d(outInnerArrLen, inInnerArrLen));
+  
+    while (result.length < g3dTier1Len) {
+      result.push(this.generate_2d(g3dTier2Len, g3dTier3Len));
     }
 
     return result;
@@ -90,3 +66,16 @@ module.exports = {
   }
 
 };
+
+function genNestArr () {
+  // expects positive numeric arguments to be passed
+  // each argument sets the number of elements. 
+  // arguments[0] > outer arguments[n] > inner
+  // genNestArr(3); === [0,1,1]
+  // genNestArr(2,3); === [[1,1,0],[0,1,0]]
+  // genNestArr(2,3,4); === [[[0,1,0,1],[1,0,1,0],[1,1,1,1]],[[0,0,0,0],[1,0,0,1],[1,0,1,0]]]
+}
+
+
+
+
--- test/test-generator.js
+++ test/test-generator.js
@@ -23,7 +23,16 @@ describe("Call MultiDimensionalArray.generate_1d method with ⇒", function() {
   it("⇒ a non-numeric argument passed. It should return an empty array.", function() {
     expect(mda.generate_1d('potato')).to.deep.equal([]);
     // console.log('result', mda.generate_1d('potato'));
-    
+  });
+
+  it("⇒ a numeric argument passed. The values contained in the array should be either 0 or 1", function() {
+    var testResult = mda.generate_1d(6);
+
+    for (var i = 0; i < testResult.length; i++) {
+      expect(testResult[i]).to.satisfy(function (val) {
+        return val === 0 || val === 1;
+      });
+    }
   });
 
   it("⇒ one numeric argument mutiple time. It should generate arrays that are not equal (i.e. random).", function() {
@@ -53,42 +62,42 @@ describe("Call MultiDimensionalArray.generate_2d method with ⇒", function() {
 
   });
 
-  it("⇒ one numeric argument (n). It should return an array length (n), containing arrays that are length (n) containing random boolean values.", function() {
-    var singleNumArg = 5;
-    var innerArrayLen = 3;
-    var testResult = mda.generate_2d(singleNumArg);
+  it("⇒ one numeric argument (n). It should return an array length (n), containing arrays that are length (3) containing random boolean values.", function() {
+    var tier1Arg = 5;
+    var tier2Arg = 3;
+    var testResult = mda.generate_2d(tier1Arg);
 
-    expect(testResult).to.have.length(singleNumArg);
+    expect(testResult).to.have.length(tier1Arg);
 
     for (var i = 0; i < testResult.length; i++) {
-      expect(testResult[i]).to.have.length(innerArrayLen);
+      expect(testResult[i]).to.have.length(tier2Arg);
     }
 
   });
 
   it("⇒ two numeric arguments (n, m). It should return an array length (n), containing arrays that are length (m)", function() {
-    var firstNumArg = 5;
-    var secondNumArg = 4;
-    var testResult = mda.generate_2d(firstNumArg, secondNumArg);
+    var tier1Arg = 5;
+    var tier2Arg = 4;
+    var testResult = mda.generate_2d(tier1Arg, tier2Arg);
 
-    expect(testResult).to.have.length(firstNumArg);
+    expect(testResult).to.have.length(tier1Arg);
 
     for (var i = 0; i < testResult.length; i++) {
-      expect(testResult[i]).to.have.length(secondNumArg);
+      expect(testResult[i]).to.have.length(tier2Arg);
     }
   });
 
   it("⇒ an argument containing a string. It should return an empty array.", function() {
-    var stringArg = 'potato';
-    var testResult = mda.generate_2d(stringArg);
+    var tier1String = 'potato';
+    var testResult = mda.generate_2d(tier1String);
 
     expect(testResult).to.deep.equal([]);
   });
 
   it("⇒ two arguments with the second containing a string. It should return an empty array.", function() {
-    var firstNumArg = '5';
-    var stringArg = 'potato';
-    var testResult = mda.generate_2d(firstNumArg, stringArg);
+    var tier1Arg = '5';
+    var tier2String = 'potato';
+    var testResult = mda.generate_2d(tier1Arg, tier2String);
 
     expect(testResult).to.deep.equal([]);
   });
@@ -101,58 +110,58 @@ describe("Call MultiDimensionalArray.generate_3d method with ⇒", function() {
     var defaultLength = 3;
     var testResult = mda.generate_3d();
     
-    expect(testResult).to.have.length(3);
+    expect(testResult).to.have.length(defaultLength);
 
     for (var i = 0; i < testResult.length; i++) {
-      expect(testResult[i]).to.have.length(3);
+      expect(testResult[i]).to.have.length(defaultLength);
       for (var j = 0; j < testResult[i].length; j++) {
-        expect(testResult[i][j]).to.have.length(3);
+        expect(testResult[i][j]).to.have.length(defaultLength);
       }
     }
   });
 
   it("⇒ one numeric argument (n). It should return an array length (n), containing arrays that are length (3) containing arrays that are length (3) that contain random boolean values.", function() {
-    var singleNumArg = 5;
-    var innerArrayLen = 3;
-    var testResult = mda.generate_3d(singleNumArg);
-
-    expect(testResult).to.have.length(singleNumArg);
+    var tier1Arg = 5;
+    var defaultLength = 3;
+    var testResult = mda.generate_3d(tier1Arg);
+    expect(testResult).to.have.length(tier1Arg);
 
     for (var i = 0; i < testResult.length; i++) {
-      expect(testResult[i]).to.have.length(3);
+      expect(testResult[i]).to.have.length(defaultLength);
       for (var j = 0; j < testResult[i].length; j++) {
-        expect(testResult[i][j]).to.have.length(3);
+        expect(testResult[i][j]).to.have.length(defaultLength);
       }
     }
   });
 
   it("⇒ two numeric arguments (n, m). It should return an array length (n), containing arrays that are length (m) containing arrays that are length (3) that contain random boolean values.", function() {
-    var firstNumArg = 5;
-    var secondNumArg = 4;
-    var testResult = mda.generate_3d(firstNumArg, secondNumArg);
+    var tier1Arg = 5;
+    var tier2Arg = 4;
+    var defaultLength = 3;
+    var testResult = mda.generate_3d(tier1Arg, tier2Arg);
 
-    expect(testResult).to.have.length(firstNumArg);
+    expect(testResult).to.have.length(tier1Arg);
 
     for (var i = 0; i < testResult.length; i++) {
-      expect(testResult[i]).to.have.length(secondNumArg);
+      expect(testResult[i]).to.have.length(tier2Arg);
       for (var j = 0; j < testResult[i].length; j++) {
-        expect(testResult[i][j]).to.have.length(3);
+        expect(testResult[i][j]).to.have.length(defaultLength);
       }
     }
   });
 
   it("⇒ three numeric arguments (n, m, o). It should return an array length (n), containing arrays that are length (m) containing arrays that are length (o) that contain random boolean values.", function() {
-    var firstNumArg = 5;
-    var secondNumArg = 4;
-    var thirdNumArg = 8;
-    var testResult = mda.generate_3d(firstNumArg, secondNumArg, thirdNumArg);
+    var tier1Arg = 5;
+    var tier2Arg = 4;
+    var tier3Arg = 8;
+    var testResult = mda.generate_3d(tier1Arg, tier2Arg, tier3Arg);
 
-    expect(testResult).to.have.length(firstNumArg);
+    expect(testResult).to.have.length(tier1Arg);
 
     for (var i = 0; i < testResult.length; i++) {
-      expect(testResult[i]).to.have.length(secondNumArg);
+      expect(testResult[i]).to.have.length(tier2Arg);
       for (var j = 0; j < testResult[i].length; j++) {
-        expect(testResult[i][j]).to.have.length(thirdNumArg);
+        expect(testResult[i][j]).to.have.length(tier3Arg);
       }
     }
   });
@@ -165,18 +174,18 @@ describe("Call MultiDimensionalArray.generate_3d method with ⇒", function() {
   });
 
   it("⇒ two arguments, the first containing a number and the second containing a string. It should return an empty array.", function() {
-    var firstNumArg = 5;
+    var tier1Arg = 5;
     var stringArg = 'potato';
-    var testResult = mda.generate_3d(firstNumArg, stringArg);
+    var testResult = mda.generate_3d(tier1Arg, stringArg);
 
     expect(testResult).to.deep.equal([]);
   });
 
   it("⇒ three arguments, the first and second containing numbers and the third containing a string. It should return an empty array.", function() {
-    var firstNumArg = 5;
-    var secondNumArg = 6;
+    var tier1Arg = 5;
+    var tier2Arg = 6;
     var stringArg = 'potato';
-    var testResult = mda.generate_3d(firstNumArg, secondNumArg, stringArg);
+    var testResult = mda.generate_3d(tier1Arg, tier2Arg, stringArg);
 
     expect(testResult).to.deep.equal([]);
   });
